94a1f9d867a239c190e07136af899701649b1726
baikonyserbolatov-blip <baikonyserbolatov@gmail.com>
2025-12-17 22:10:54 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,12 +9,10 @@ import bookingsRoutes from "./routes/bookingsRoutes.js";
 import newsRoutes from "./routes/newsRoutes.js";
 
 dotenv.config();
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
@@ -25,4 +23,5 @@ app.use("/api/courses", coursesRoutes);
 app.use("/api/bookings", bookingsRoutes);
 app.use("/api/news", newsRoutes);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
