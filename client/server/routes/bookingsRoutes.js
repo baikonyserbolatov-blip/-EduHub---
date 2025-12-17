a7a3582b25f241c24a249e83ba9fa3596ef28fc3
baikonyserbolatov-blip <baikonyserbolatov@gmail.com>
2025-12-17 21:59:26 +0500

    Delete client/server directory

--- client/server/routes/bookingsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getBookings, createBooking } from "../controllers/bookingsController.js";
-
-const router = express.Router();
-
-router.get("/:userId", getBookings);
-router.post("/", createBooking);
-
-export default router;
--- client/server/routes/coursesRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getCourses, getCourseById } from "../controllers/coursesController.js";
-
-const router = express.Router();
-
-router.get("/", getCourses);
-router.get("/:id", getCourseById);
-
-export default router;
